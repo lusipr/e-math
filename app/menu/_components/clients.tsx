0ef3243f0f6e2ed79594f1828ec8fdf15e5db037
lusipr <lusipr142@gmail.com>
2026-05-05 08:50:31 +0700

    fixs url

--- app/menu/_components/clients.tsx
+++ app/menu/_components/clients.tsx
@@ -4,17 +4,17 @@ import { useRouter } from "next/navigation";
 import Image from "next/image";
 
 const topMenu = [
-  { img: "/assets/images/cptp.png", path: "/materi/cp-tp" },
+  { img: "/assets/images/cptp.png", path: "/materi/tp-cp" },
   { img: "/assets/images/pengantar.png", path: "/materi/pengantar-spldv" },
 ];
 
 const bottomMenu = [
   { img: "/assets/images/metode-grafik.png", path: "/materi/metode-grafik" },
-  { img: "/assets/images/substitusi.png", path: "/materi/metode-substitusi" },
+  { img: "/assets/images/substitusi.png", path: "/materi/metode-subtitusi" },
   { img: "/assets/images/eliminasi.png", path: "/materi/metode-eliminasi" },
   { img: "/assets/images/campuran.png", path: "/materi/metode-campuran" },
-  { img: "/assets/images/latihan-soal.png", path: "/materi/latihan-soal" },
-  { img: "/assets/images/profile.png", path: "/profil" },
+  { img: "/assets/images/latihan-soal.png", path: "/quiz/metode-grafik" },
+  { img: "/assets/images/profile.png", path: "/profile" },
 ];
 
 export default function Client() {
